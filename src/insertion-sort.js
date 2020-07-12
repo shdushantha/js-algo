f58791349f5248055dac8fb5b49e6a644a172daa
dushantha <shdushantha@gmail.com>
2020-07-12 08:44:13 +0800

    insertion sort

---
+++ src/insertion-sort.js
@@ -0,0 +1,17 @@
+const insertionSort = array => {
+    const length = array.length;
+
+    for(let i=1; i<length;i++){
+        let key = array[i];
+        let j=i-1;
+
+        while(j>=0 && key < array[j]){
+            array[j+1] = array[j];
+            j = j-1;
+        }
+        array[j+1]=key;
+    }
+    return array;
+};
+
+console.log(insertionSort([7,5,1,8,2,3]));
